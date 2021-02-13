0e881bcdbfab783f836ea642b50296a9a9553f44
peterson1999 <anjunieco@gmail.com>
2021-02-13 19:11:25 +0800

    ddfdsf

---
+++ BackProp/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace BackProp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.createNNToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Learn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createNNToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // createNNToolStripMenuItem
+            // 
+            this.createNNToolStripMenuItem.Name = "createNNToolStripMenuItem";
+            this.createNNToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.createNNToolStripMenuItem.Text = "Create NN";
+            this.createNNToolStripMenuItem.Click += new System.EventHandler(this.createNNToolStripMenuItem_Click);
+            // 
+            // Learn
+            // 
+            this.Learn.Location = new System.Drawing.Point(38, 42);
+            this.Learn.Name = "Learn";
+            this.Learn.Size = new System.Drawing.Size(75, 23);
+            this.Learn.TabIndex = 1;
+            this.Learn.Text = "Learn";
+            this.Learn.UseVisualStyleBackColor = true;
+            this.Learn.Click += new System.EventHandler(this.Learn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(363, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(552, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(179, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(179, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Learn);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem createNNToolStripMenuItem;
+        private System.Windows.Forms.Button Learn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BackProp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Backprop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackProp
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet nn;
+        string [] words;
+        int[] bagofwords = { 0, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void createNNToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(2, 20, 2);
+
+        }
+
+        private void Learn_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                //Love
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.setDesiredOutput(1, 1.0);
+                nn.learn();
+                //hate
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.setDesiredOutput(1, 0.0);
+                nn.learn();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bagofwords[0]= 0;
+            bagofwords[0] = 0;
+            words = textBox1.Text.ToLower().Split(' ');
+            foreach (var word in words)
+            {
+                //System.Console.WriteLine($"<{word}>");
+                if (word =="hate")
+                {
+                    bagofwords[1]+=1;
+                }
+                else if(word =="love")
+                {
+                    bagofwords[0] +=1;
+                }
+            }
+
+            nn.setInputs(0, Convert.ToDouble(bagofwords[0]));
+            nn.setInputs(1, Convert.ToDouble(bagofwords[1]));
+            nn.run();
+            label1.Text = "Love: " + nn.getOuputData(1).ToString();
+            label2.Text = "Hate: " + nn.getOuputData(0).ToString();
+            /* if (nn.getOuputData(0) == 1)
+            {
+                label1.Text = "Hate";
+            }
+
+            else if (nn.getOuputData(0) == 0)
+            {
+                label1.Text = "Love";
+            }
+            */
+
+            //label1.Text = "Love: " + bagofwords[0].ToString() + " Hate: " + bagofwords[1].ToString();
+        }
+    }
+}
